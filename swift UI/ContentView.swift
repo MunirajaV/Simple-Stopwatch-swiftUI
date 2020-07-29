50e38b3633e950ec023db277a303fc6c87223f5f
muniraja <muniraja@sparkouttech.com>
2020-07-29 17:31:07 +0530

    Initial Commit

---
+++ swift UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift UI
+//
+//  Created by Muniraja Velumani on 29/07/20.
+//  Copyright © 2020 Muniraja Velumani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
