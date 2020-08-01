725907664d7d95b6757ac2082f53bdbd12a24a56
muniraja <muniraja@sparkouttech.com>
2020-08-01 11:26:50 +0530

    added screens

--- swift UI/ContentView.swift
+++ swift UI/ContentView.swift
@@ -9,13 +9,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var stopWatchManager = Stopwatch()
+    
     var body: some View {
-        Text("Hello, World!")
+//        Text("Hello, World!")
+        VStack {
+            Text(String(format: "%.1f", stopWatchManager.seconds))
+                .font(.custom("Avenir", size: 80))
+                .fontWeight(.semibold)
+                .padding(.top, 50)
+                .padding(.bottom, 50)
+            if stopWatchManager.mode == .stopped {
+                Button(action: {self.stopWatchManager.start()}) {
+                    TimerButton(label: "Start", buttonColor: .green)
+                }
+            }
+            if stopWatchManager.mode == .running {
+                Button(action: {self.stopWatchManager.pause()}) {
+                    TimerButton(label: "Pause", buttonColor: .blue)
+                }
+                Button(action: {self.stopWatchManager.stop()}) {
+                    TimerButton(label: "Stop", buttonColor: .red)
+                }
+                .padding(.top, 30)
+            }
+            if stopWatchManager.mode == .paused {
+                Button(action: {self.stopWatchManager.start()}) {
+                    TimerButton(label: "Start", buttonColor: .blue)
+                }
+                Button(action: {self.stopWatchManager.stop()}) {
+                    TimerButton(label: "Stop", buttonColor: .red)
+                }
+                    .padding(.top, 30)
+            }
+            Spacer()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct TimerButton: View {
+    
+    let label: String
+    let buttonColor: Color
+    
+    var body: some View {
+        Text(label)
+            .font(.custom("Avenir", size: 25))
+            .foregroundColor(.white)
+            .frame(width: 150, height: 150, alignment: .center)
+            .background(buttonColor)
+            .clipShape(Circle())
+            .padding(3)
+            .overlay(
+            RoundedRectangle(cornerRadius: 100)
+                .stroke(buttonColor, lineWidth: 2))
     }
 }
--- swift UI/Stopwatch.swift
+++ swift UI/Stopwatch.swift
@@ -6,4 +6,37 @@
 //  Copyright © 2020 Muniraja Velumani. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+enum stopWatchMode {
+    case running
+    case stopped
+    case paused
+}
+
+class Stopwatch: ObservableObject {
+    
+    @Published var mode: stopWatchMode = .stopped
+    @Published var seconds = 0.0
+    
+    var timer = Timer()
+    
+    func start() {
+        mode = .running
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+            self.seconds = self.seconds + 0.1
+        }
+    }
+    
+    func pause() {
+        timer.invalidate()
+        mode = .paused
+    }
+    
+    func stop() {
+        timer.invalidate()
+        seconds = 0
+        mode = .stopped
+    }
+    
+}
